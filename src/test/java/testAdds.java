8513d97476be86dd7f931dc850677de81423f4ac
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-20 13:51:53 -0400

    Add files via upload

---
+++ src/test/java/testAdds.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testAdds {
+	@test
+	public void testAdd1Plus1() {
+		int answer = 1;
+		
+		AddsNum add = new AddsNum();
+		assertEquals("string", "string");
+	}
+}
