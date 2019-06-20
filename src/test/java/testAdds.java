2f8287c164355c51b6b43be53b264a85ce7e3bfe
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-20 14:05:40 -0400

    Update testAdds.java

--- src/test/java/testAdds.java
+++ src/test/java/testAdds.java
@@ -1,4 +1,5 @@
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,9 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class testAdds {
 	@Test
 	public void testAdd1Plus1() {
-		int answer = 1;
+		int answer = 2;
 		
 		AddsNum add = new AddsNum();
-		assertEquals("string", "string");
+		assertEquals(answer, add.adds(1,1));
 	}
 }
