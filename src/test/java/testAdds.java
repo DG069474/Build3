aba0ac99103558cce5a69473fd364a57c2ba8a65
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-24 15:26:05 -0400

    Update testAdds.java

--- src/test/java/testAdds.java
+++ src/test/java/testAdds.java
@@ -9,15 +9,15 @@ public class testAdds {
 	public void testAdd1Plus1() {
 		int answer = 2;
 		
-		AddsNum add = new AddsNum();
-		assertEquals(answer, add.adds(1,1));
+		AddsNum add = new AddsNum(1,1);
+		assertEquals(answer, add.adds());
 	}
 	
 	@Test
 	public void testAddwrong() {
 		int answer = 1;
 		
-		AddsNum add = new AddsNum();
-		assertFalse(answer == add.adds(1,1));
+		AddsNum add = new AddsNum(1,1);
+		assertFalse(answer == add.adds());
 	}
 }
