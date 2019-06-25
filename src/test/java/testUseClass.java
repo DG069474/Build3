94dd39339fee1221338a5fc7d11bf97c226e19a3
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-25 08:52:53 -0400

    Update testUseClass.java

--- src/test/java/testUseClass.java
+++ src/test/java/testUseClass.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class testUseClass {
 	@Test
 	public void testUse() {
-		useAdds use = new UseAdds();
+		useAdds use = new useAdds();
     		use.setNums();
     		int[] nums = use.getNums();
     		int answer = nums[0] + nums[1];
