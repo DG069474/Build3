53c7e2064e115102f1a24af75b1b72e5f9143491
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-25 08:52:07 -0400

    Update and rename testUse.java to testUseClass.java

--- src/test/java/testUseClass.java
+++ src/test/java/testUseClass.java
@@ -2,13 +2,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class testAdds {
+public class testUseClass {
 	@Test
 	public void testUse() {
 		useAdds use = new UseAdds();
-    use.setNums();
-    int[] nums = use.getNums();
-    int answer = nums[0] + nums[1];
+    		use.setNums();
+    		int[] nums = use.getNums();
+    		int answer = nums[0] + nums[1];
     
 		assertEquals(answer, use.use());
 	}
